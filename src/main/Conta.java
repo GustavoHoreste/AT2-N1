4076f90315c276cdec74c122be05e85070e51beb
GustavoHoreste <101297032+GustavoHoreste@users.noreply.github.com>
2024-04-25 19:36:22 -0300

    classes adicionadas

---
+++ src/main/Conta.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Conta {
+	private String nome;
+	private double saldo;
+
+	
+	public String getnome() {
+		return nome;
+	}
+	
+	public void setnome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getsaldo() {
+		return saldo;
+	}
+	
+	public void setsaldo(double saldo) {
+		this.saldo = saldo;
+	}
+}
--- src/main/Home.java
+++ src/main/Home.java
@@ -3,8 +3,13 @@ package main;
 public class Home {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-		System.out.println("Hello, World!");
-		System.out.println("Hello, World!");
+		
+		Conta conta1 = new Conta();
+		
+		conta1.setnome("gustavo");
+		
+		System.out.println(conta1.getnome());
+		
+		System.out.println("Terminei execucao");
 	}
 }
